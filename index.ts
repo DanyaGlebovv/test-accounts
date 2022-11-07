c667eca373a9df659b553a00a19461731aefa076
DanyaGlebovv <daniil.m.glebov@gmail.com>
2022-11-07 22:36:22 +0200

    fix: update startup files

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
 
 import { init } from './src';
+import config from '@ebdp1/config-lib';
 import fastify from 'fastify';
 
 const server = fastify();
 
-init(server).listen({ port: 8080 }, (err, address) => {
+init(server).listen({ port: config.get('port') || 8080 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
--- local.ts
+++ local.ts
@@ -1,1 +1,15 @@
-export const a = '';
+import 'reflect-metadata';
+
+import { init } from './src';
+import config from '@ebdp1/config-lib';
+import fastify from 'fastify';
+
+const server = fastify();
+
+init(server).listen({ port: config.get('port') || 8080 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
