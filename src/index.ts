4424f335647182461c6b0975ecf1e8347bdad12a
DanyaGlebovv <daniil.m.glebov@gmail.com>
2022-11-10 22:56:12 +0200

    Added nodemon & secrets

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import fastify from 'fastify';
 import AutoLoad from 'fastify-autoload';
 import { FastifyReply } from 'fastify';
-import jsonwebtoken from 'jsonwebtoken';
+import authLib from '@ebdp1/auth-lib';
+import config from '@ebdp1/config-lib';
 import path from 'path';
 import { AppDataSource } from './db';
 import { CustomFastifyRequest, UserDataJWT } from 'models';
@@ -9,6 +10,7 @@ import 'reflect-metadata';
 
 export const init = async (opts?) => {
   const app = fastify();
+
   await AppDataSource.initialize();
   console.log('Database connect success');
 
@@ -18,12 +20,16 @@ export const init = async (opts?) => {
     options: Object.assign({}, opts),
   });
 
+  authLib.initialize(config);
+  const { tokenService } = authLib;
+
   app.decorate('authenticate', async function (request: CustomFastifyRequest, reply: FastifyReply) {
     try {
       if (!request.headers.authorization) {
         reply.status(401).send('Unauthorized');
       }
-      const user = jsonwebtoken.decode(request.headers.authorization);
+      const user = await tokenService.decodeToken(request.headers.authorization);
+      console.log(user);
       request.user = user as UserDataJWT;
     } catch (err) {
       reply.send(err);
